49f92fef67cb6335c576e628f79462eb8d770a3d
Ion Gorincioi <gorincioii@yahoo.com>
2016-07-05 10:32:54 -0700

    Solution for array permutations complete

--- arrayPermutator.js
+++ arrayPermutator.js
@@ -1,20 +1,17 @@
-function permutator (str) { //arr
-  //var arr = str.split("");
-  var permutations = [];
-  if (str.length === 1) { //arr
-    return [ str ];   //arr
+function permutator (arr) {
+  
+  let permutations = [];
+  if (arr.length === 1) {
+    return [arr];
   }
-
-  for (var i = 0; i <  str.length; i++) { 
-    // var subPerms = permutator((arr.slice(0, i).concat(arr.slice(i + 1))).join(""));
-    var subPerms = permutator(str.substr(0, i).concat(str.substr(i + 1)));
-    for (var j = 0; j < subPerms.length; j++) {
-      //subPerms[j].unshift(arr[i]);
-      var sub = str[i].concat(subPerms[j]);
-      permutations.push(sub);
+  for (let i = 0; i < arr.length; i++) { 
+    let subPerms = permutator(arr.slice(0, i).concat(arr.slice(i + 1)));
+    for (let j = 0; j < subPerms.length; j++) {
+      subPerms[j].unshift(arr[i]);
+      permutations.push(subPerms[j]);
     }
   }
-  return permutations;
-  
+  return permutations; 
 }
-permutator("abcd");
+
+//permutator(["a", "b", "c", "d"]);
