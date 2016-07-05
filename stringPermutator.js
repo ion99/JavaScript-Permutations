b782c47a09a871b0cfb97ceeaaf645c22ab17234
Ion Gorincioi <gorincioii@yahoo.com>
2016-07-05 10:36:03 -0700

    ES6

--- stringPermutator.js
+++ stringPermutator.js
@@ -1,14 +1,14 @@
 function permutator (str) { 
 
-  var permutations = [];
+  let permutations = [];
   if (str.length === 1) { 
     return [ str ];
   }
 
-  for (var i = 0; i <  str.length; i++) { 
-    var subPerms = permutator(str.substr(0, i).concat(str.substr(i + 1)));
-    for (var j = 0; j < subPerms.length; j++) {
-      var sub = str[i].concat(subPerms[j]);
+  for (let i = 0; i <  str.length; i++) { 
+    let subPerms = permutator(str.substr(0, i).concat(str.substr(i + 1)));
+    for (let j = 0; j < subPerms.length; j++) {
+      let sub = str[i].concat(subPerms[j]);
       permutations.push(sub);
     }
   }
